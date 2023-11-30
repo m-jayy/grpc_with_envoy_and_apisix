5d4dc926c0d6d08f6a66683e5891c69e6ef1c2b3
Affan Amir Mir <affan.amir@din.global>
2023-11-30 13:10:40 +0500

    some commits

--- lib/common/constants/constants.dart
+++ lib/common/constants/constants.dart
@@ -3,5 +3,5 @@ abstract class AppConstants {
   static const apiEndpointCrypto = "https://api.coinpaprika.com/";
 
 static const host = '127.0.0.1';
-static const port = 9081;
+static const port = 9080;
 }
